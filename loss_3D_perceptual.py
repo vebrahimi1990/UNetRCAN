dea680f537a09fdb173d06b390876a9c30cdf3a3
Vahid Ebrahimi <95882710+vebrahimi1990@users.noreply.github.com>
2022-11-07 11:27:29 -0500

    remove en_out, de_out from model_2d_rcan

--- loss_3D_perceptual.py
+++ loss_3D_perceptual.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 from keras.models import Input, Model, load_model
 
 patch_size = 160
@@ -77,12 +76,13 @@ def perceptual_loss(prediction, gt):
 #     return loss
 
 
-def generator_loss(prediction1, prediction2, gt):
+def generator_loss(prediction, gt):
+    prediction1, prediction2 = prediction
     norm_mse1 = norm_mse_loss(prediction1, gt)
     percept_loss1 = perceptual_loss(prediction1, gt)
     norm_mse2 = norm_mse_loss(prediction2, gt)
-    sim_loss2 = ssim_loss(prediction2,gt)
-    gen_loss1 = norm_mse1 + 0.01*percept_loss1
+    sim_loss2 = ssim_loss(prediction2, gt)
+    gen_loss1 = norm_mse1 + 0.01 * percept_loss1
     gen_loss2 = norm_mse2 + sim_loss2
     total_gen_loss = gen_loss1 + gen_loss2
     return total_gen_loss
